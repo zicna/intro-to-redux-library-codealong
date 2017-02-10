7681b2b5127ce71ef038d605ec34de85803653ce
Luke Ghenco <lukeghenco@gmail.com>
2017-02-10 09:42:07 -0500

    Update App.js solution

--- src/App.js
+++ src/App.js
@@ -1,35 +1,24 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import './App.css';
 
 class App extends Component {
-	handleOnClick = event => {
-		this.props.increaseCount();
-	};
 
-	render() {
-		return (
-			<div className="App">
-				<button onClick={this.handleOnClick}>Click</button>
-				<p>{this.props.items.length}</p>
-			</div>
-		);
-	}
-}
+  handleOnClick() {
+    this.props.store.dispatch({
+      type: 'INCREASE_COUNT',
+    });
+  }
 
-const mapStateToProps = state => {
-	return {
-		items: state.items
-	};
+  render() {
+    return (
+      <div className="App">
+        <button onClick={(event) => this.handleOnClick(event)} >
+          Click 
+        </button>
+        <p>{this.props.store.getState().items.length}</p>
+      </div>
+    );
+  }
 };
 
-const mapDispatchToProps = dispatch => {
-	return {
-		increaseCount: () => dispatch({ type: 'INCREASE_COUNT' })
-	};
-};
-
-export default connect(
-	mapStateToProps,
-	mapDispatchToProps
-)(App);
+export default App;
