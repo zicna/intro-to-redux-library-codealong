f2124d4ea6238e596e46dbbbe66c8f4aced16239
lizbur10 <liz.burton147@gmail.com>
2020-07-26 16:33:57 -0400

    changed code in App.js to work without Provider/connect

--- src/App.js
+++ src/App.js
@@ -1,35 +1,22 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import './App.css';
 
 class App extends Component {
-	handleOnClick = event => {
-		this.props.increaseCount();
-	};
+	handleOnClick = () => {
+		this.props.store.dispatch({
+		  type: 'INCREASE_COUNT',
+		});
+	  }
 
 	render() {
+		const state = this.props.store.getState();
 		return (
 			<div className="App">
 				<button onClick={this.handleOnClick}>Click</button>
-				<p>{this.props.items.length}</p>
+				<p>{state.items.length}</p>
 			</div>
 		);
 	}
 }
 
-const mapStateToProps = state => {
-	return {
-		items: state.items
-	};
-};
-
-const mapDispatchToProps = dispatch => {
-	return {
-		increaseCount: () => dispatch({ type: 'INCREASE_COUNT' })
-	};
-};
-
-export default connect(
-	mapStateToProps,
-	mapDispatchToProps
-)(App);
+export default App;
